d1169b6512e81a2ab4c8f65053d10f5dfc0ed09e
HemanthGk2004 <gkhemanth0@gmail.com>
2025-04-09 01:31:38 +0530

    Removed password

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,6 @@ const userSchema = new mongoose.Schema({
     __id:{type: String, required: true},
     name: { type: String, required: true },
     email: { type: String, required: true,unique: true },
-    password: { type: String, required: true },
     resume: { type: String },
     image: { type: String, required: true }
 }, { timestamps: true });
